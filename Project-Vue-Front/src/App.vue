a8c57db4a46dba9da158b9ea3b3ccd0725f47c28
Allan Furlani Nishigouri <anishigouri@mbp-de-allan.home>
2018-01-15 21:25:04 -0200

    Projeto inicial VueJS

---
+++ Project-Vue-Front/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li :key="foto.titulo" v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo" />
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      titulo: 'Alurapic',
+      fotos: [
+        {
+          url: 'https://meusanimais.com.br/wp-content/uploads/2015/05/gato.jpg',
+          titulo: 'Gato'
+        },
+        {
+          url: 'https://meusanimais.com.br/wp-content/uploads/2015/05/gato.jpg',
+          titulo: 'Gatao'
+        }
+      ]
+    }
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
